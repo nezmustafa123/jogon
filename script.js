46808312fd4b4d081a2db6a81bc0f2836367e973
Nez Mustafa <nezmustafa123@gmail.com>
2021-10-02 16:03:14 +0100

    leaflet library continued

--- script.js
+++ script.js
@@ -23,14 +23,17 @@ if (navigator.geolocation) {
       const { longitude } = position.coords;
       //destructure longitude
       console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
-      const map = L.map('map').setView([51.505, -0.09], 13);
 
-      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      const coords = [latitude, longitude];
+
+      const map = L.map('map').setView(coords, 14);
+      //l variable global variable that's available in other scripts only works if scrpts file comes after
+      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      L.marker([51.5, -0.09])
+      L.marker(coords)
         .addTo(map)
         .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
         .openPopup();
